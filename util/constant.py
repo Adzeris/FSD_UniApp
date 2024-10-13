0f6a4f7f8aab85e4e3396a03d60e8197cd1a081f
Md Saadman Kabir <saadman97@gmail.com>
2024-10-13 15:20:11 +1100

    Update 1
    Copied from Unidemo

---
+++ util/constant.py
@@ -0,0 +1,12 @@
+from enum import Enum, auto
+
+
+class Constant(Enum):
+    # define general constant class to store static value
+
+    # type 1: KEYs for return map in service layer
+    KEY_SUBJECT_ID = "subject_id"
+    KEY_COUNT = "key_count"
+
+
+    
