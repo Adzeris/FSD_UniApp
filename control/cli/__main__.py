f63c2f92c46f86d9cc25b04b06657d033b18f561
Md Saadman Kabir <saadman97@gmail.com>
2024-10-13 17:48:02 +1100

    3

--- control/cli/__main__.py
+++ control/cli/__main__.py
@@ -1,5 +1,8 @@
 # control/cli/__main__.py
-from control.cli.cli_uni_app import main
+try:
+    from .cli_uni_app import main  # Relative import
+except ImportError as e:
+    print(f"ImportError: {e}")
 
 if __name__ == "__main__":
     main()
