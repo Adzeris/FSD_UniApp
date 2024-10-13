5bef76d9a369d71aa61ed0f1267e59ed1c54077f
Md Saadman Kabir <saadman97@gmail.com>
2024-10-13 16:48:45 +1100

    1

--- control/cli/admin_control.py
+++ control/cli/admin_control.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 from service.admin_service import AdminService
 from util.util import input_cyan
 
